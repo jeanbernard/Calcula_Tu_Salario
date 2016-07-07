f16b0ce27b08599965056f389aac8d68cd012f78
Jean Bernard <jeanmarinobernard@gmail.com>
2016-07-06 20:39:46 -0400

    Uncommented code for Payroll struct and tests.

--- Calcula Tu Salario/Payroll.swift
+++ Calcula Tu Salario/Payroll.swift
@@ -1,24 +1,24 @@
-//import Foundation
-//
-//struct Payroll {
-//  
-//  static func calculateAFP_SFS(salary: NSDecimalNumber) -> NSDecimalNumber {
-//    let salaryAfterAFP_SFS = Deduction.applyGovernmentTaxesToSalary(salary)
-//    return salaryAfterAFP_SFS
-//  }
-//  
-//  static func calculateMonthlyNetSalary(salary: NSDecimalNumber) -> NSDecimalNumber {
-//    let salaryAfterAFP_SFS = Deduction.applyGovernmentTaxesToSalary(salary)
-//    let salaryISRDeductionAmount = ISR.getMontlyRetentionAmount(salaryAfterAFP_SFS)
-//    let netSalary: NSDecimalNumber = salaryAfterAFP_SFS - salaryISRDeductionAmount
-//    
-//    return netSalary
-//  }
-//  
-//  static func calculateBiWeeklyNetSalary(salary: NSDecimalNumber) -> NSDecimalNumber {
-//    let calculateSalary = calculateMonthlyNetSalary(salary)
-//    return NSDecimalNumber.roundToNearestTwo(calculateSalary / 2)
-//
-//  }
-//  
-//}
+import Foundation
+
+struct Payroll {
+  
+  static func calculateAFP_SFS(salary: NSDecimalNumber) -> NSDecimalNumber {
+    let salaryAfterAFP_SFS = Deduction.applyGovernmentTaxesToSalary(salary)
+    return salaryAfterAFP_SFS
+  }
+  
+  static func calculateMonthlyNetSalary(salary: NSDecimalNumber) -> NSDecimalNumber {
+    let salaryAfterAFP_SFS = Deduction.applyGovernmentTaxesToSalary(salary)
+    let salaryISRDeductionAmount = ISR.getMontlyRetentionAmount(salaryAfterAFP_SFS)
+    let netSalary: NSDecimalNumber = salaryAfterAFP_SFS - salaryISRDeductionAmount
+    
+    return netSalary
+  }
+  
+  static func calculateBiWeeklyNetSalary(salary: NSDecimalNumber) -> NSDecimalNumber {
+    let calculateSalary = calculateMonthlyNetSalary(salary)
+    return NSDecimalNumber.roundToNearestTwo(calculateSalary / 2)
+
+  }
+  
+}
