71ae864a25e9af8354613699c646466f36d823bb
Jean Bernard <jeanmarinobernard@gmail.com>
2016-07-26 06:10:23 -0400

    Added table view to the results view controller. Also the custom cell that will show the detail of the result.

---
+++ Calcula Tu Salario/ResultTableViewCell.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var amountLabel: UILabel!
+  
+
+}
--- Calcula Tu Salario/ResultsViewController.swift
+++ Calcula Tu Salario/ResultsViewController.swift
@@ -5,9 +5,33 @@ class ResultsViewController: UIViewController {
   var result: NSDecimalNumber = 0.0
   
   @IBOutlet weak var resultLabel: UILabel!
+  @IBOutlet weak var resultsTableView: UITableView!
   
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        resultLabel.text = "$\(result)"
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    resultsTableView.dataSource = self
+    resultLabel.text = "$\(result)"
+  }
+}
+
+extension ResultsViewController: UITableViewDataSource {
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+  
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+    let cell = tableView.dequeueReusableCellWithIdentifier("resultCell") as? ResultTableViewCell
+    
+    if indexPath.section == 0 {
+      cell?.titleLabel.text = "Salario"
+      cell?.amountLabel.text = "$10,000.00"
     }
+    
+    return cell!
+  }
 }
