0a92209b245a2fa4c61cade4ad203cf22f142750
Jean Bernard <jeanmarinobernard@gmail.com>
2016-07-31 14:28:12 -0400

    Refactored function names.

--- Calcula Tu Salario/View Models/SalaryViewModel.swift
+++ Calcula Tu Salario/View Models/SalaryViewModel.swift
@@ -7,16 +7,16 @@ struct SalaryViewModel {
   var deductions: [String: String] = [:]
   
   init(salary: NSDecimalNumber) {
-    self.netSalary = formatNumberToCurrency(Payroll.calculateMonthlyNetSalary(salary))
-    self.income = formatNumberToCurrency(salary)
-    self.deductions = formatDictionaryToCurrency(Payroll.obtainAllDeductions(forSalary: salary))
+    self.netSalary = formatNumberToCurrencyString(Payroll.calculateMonthlyNetSalary(salary))
+    self.income = formatNumberToCurrencyString(salary)
+    self.deductions = formatDictionaryToCurrencyString(Payroll.obtainAllDeductions(forSalary: salary))
   }
   
   init() {
     
   }
   
-  private func formatNumberToCurrency(number: NSDecimalNumber) -> String? {
+  private func formatNumberToCurrencyString(number: NSDecimalNumber) -> String? {
     let formatter = NSNumberFormatter()
     formatter.numberStyle = .CurrencyStyle
     
@@ -26,12 +26,11 @@ struct SalaryViewModel {
     return nil
   }
   
-  private func formatDictionaryToCurrency(dictionary: [String: NSDecimalNumber]) -> [String: String] {
-
+  private func formatDictionaryToCurrencyString(dictionary: [String: NSDecimalNumber]) -> [String: String] {
     var stringyDictionary: [String: String] = [:]
     
     for (key, value) in dictionary {
-      stringyDictionary.updateValue(formatNumberToCurrency(value)!, forKey: key)
+      stringyDictionary.updateValue(formatNumberToCurrencyString(value)!, forKey: key)
     }
     return stringyDictionary
   }
