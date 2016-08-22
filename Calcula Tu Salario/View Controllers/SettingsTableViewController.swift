70cc73e2acdd0d85cef7015d46d89b383c992a6a
Jean Bernard <jeanmarinobernard@gmail.com>
2016-08-22 19:49:21 -0400

    NSUserDefaults that persists the user selection of Night Shit "Mode".

--- Calcula Tu Salario/View Controllers/SettingsTableViewController.swift
+++ Calcula Tu Salario/View Controllers/SettingsTableViewController.swift
@@ -1,23 +1,34 @@
 import UIKit
 
-class SettingsTableViewController: UITableViewController {
+protocol SettingsDelegate: class {
+  func isNightShiftEnabled(switchButton: UISwitch)
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+class SettingsTableViewController: UITableViewController {
+  
+  @IBOutlet weak var nightShiftSwitch: UISwitch!
+  weak var delegate: SettingsDelegate?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    let userDefaults = NSUserDefaults.standardUserDefaults().boolForKey("NightSwitch")
+    nightShiftSwitch.on = userDefaults
+  }
   
   
   @IBAction func cancelButtonPressed(sender: UIBarButtonItem) {
+    NSUserDefaults.standardUserDefaults().setBool(nightShiftSwitch.on, forKey: "NightSwitch")
+    delegate?.isNightShiftEnabled(nightShiftSwitch)
     dismissViewControllerAnimated(true, completion: nil)
   }
-
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
+  
+  // MARK: - Table view data source
+  
+  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
 }
