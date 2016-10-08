83bdacd5a3149dbd95a04663115cf7169bf7accb
Jean Bernard <jeanmarinobernard@gmail.com>
2016-10-08 16:14:25 -0400

    Removed Total label. Looks quite nice now :] .

--- Calcula Tu Salario/View Controllers/Delegates/ResultsVCTableViewDelegate.swift
+++ Calcula Tu Salario/View Controllers/Delegates/ResultsVCTableViewDelegate.swift
@@ -4,7 +4,7 @@ extension ResultsViewController: UITableViewDelegate {
   func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
     
     if let view = view as? UITableViewHeaderFooterView {
-      view.textLabel?.font = UIFont.systemFont(ofSize: 24, weight: UIFontWeightSemibold)
+      view.textLabel?.font = UIFont.systemFont(ofSize: 23, weight: UIFontWeightSemibold)
       //view.textLabel?.font = UIFont.preferredFont(forTextStyle: .title2)
       view.tintColor = UIColor.clear
       view.textLabel?.text = view.textLabel?.text?.capitalized
